fd2854abeda547bf563e94eb85ad04a9136749a7
RoyLin <1002591652@qq.com>
2026-02-09 18:25:15 +0800

    bump a3s-search to v0.5.0 (Baidu & Bing China re-added)

--- Formula/a3s-search.rb
+++ Formula/a3s-search.rb
@@ -1,27 +1,27 @@
 class A3sSearch < Formula
   desc "Embeddable meta search engine CLI with proxy pool support"
   homepage "https://github.com/A3S-Lab/Search"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.4.0/a3s-search-0.4.0-darwin-arm64.tar.gz"
-      sha256 "eda16fa05f1f580d15effee84912e4f94855a12cbd326654311f82ab80fd1a82"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.0/a3s-search-0.5.0-darwin-arm64.tar.gz"
+      sha256 "e76d0901386790919d3804464315ed75926b235563d914d1bf25c45ae1d09c4d"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.4.0/a3s-search-0.4.0-darwin-x86_64.tar.gz"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.0/a3s-search-0.5.0-darwin-x86_64.tar.gz"
       sha256 "PLACEHOLDER_X86_64_SHA256"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.4.0/a3s-search-0.4.0-linux-arm64.tar.gz"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.0/a3s-search-0.5.0-linux-arm64.tar.gz"
       sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.4.0/a3s-search-0.4.0-linux-x86_64.tar.gz"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.0/a3s-search-0.5.0-linux-x86_64.tar.gz"
       sha256 "PLACEHOLDER_LINUX_X86_64_SHA256"
     end
   end
