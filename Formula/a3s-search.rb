e249aca4ccb2dca9d0cde5ef82a6df0f0fa96780
RoyLin <1002591652@qq.com>
2026-02-09 23:56:39 +0800

    a3s-search: update to v0.5.1

--- Formula/a3s-search.rb
+++ Formula/a3s-search.rb
@@ -1,27 +1,27 @@
 class A3sSearch < Formula
   desc "Embeddable meta search engine CLI with proxy pool support"
   homepage "https://github.com/A3S-Lab/Search"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.0/a3s-search-0.5.0-darwin-arm64.tar.gz"
-      sha256 "e76d0901386790919d3804464315ed75926b235563d914d1bf25c45ae1d09c4d"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.1/a3s-search-0.5.1-darwin-arm64.tar.gz"
+      sha256 "cb9382650d9617e5e16cb30377cbf0ce94a4c3faaaff8cf21bc9c519da2a1f29"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.0/a3s-search-0.5.0-darwin-x86_64.tar.gz"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.1/a3s-search-0.5.1-darwin-x86_64.tar.gz"
       sha256 "PLACEHOLDER_X86_64_SHA256"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.0/a3s-search-0.5.0-linux-arm64.tar.gz"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.1/a3s-search-0.5.1-linux-arm64.tar.gz"
       sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.0/a3s-search-0.5.0-linux-x86_64.tar.gz"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.1/a3s-search-0.5.1-linux-x86_64.tar.gz"
       sha256 "PLACEHOLDER_LINUX_X86_64_SHA256"
     end
   end
