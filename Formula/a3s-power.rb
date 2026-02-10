4b6ed2a01aeff8a0c080d2370bc81a4183c3acb4
RoyLin <1002591652@qq.com>
2026-02-11 04:10:01 +0800

    chore: bump a3s-power to v0.1.1

--- Formula/a3s-power.rb
+++ Formula/a3s-power.rb
@@ -1,27 +1,27 @@
 class A3sPower < Formula
   desc "Local model management and serving with OpenAI-compatible API"
   homepage "https://github.com/A3S-Lab/Power"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/A3S-Lab/Power/releases/download/v0.1.0/a3s-power-0.1.0-darwin-arm64.tar.gz"
+      url "https://github.com/A3S-Lab/Power/releases/download/v0.1.1/a3s-power-0.1.1-darwin-arm64.tar.gz"
       sha256 "PLACEHOLDER_DARWIN_ARM64_SHA256"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/Power/releases/download/v0.1.0/a3s-power-0.1.0-darwin-x86_64.tar.gz"
+      url "https://github.com/A3S-Lab/Power/releases/download/v0.1.1/a3s-power-0.1.1-darwin-x86_64.tar.gz"
       sha256 "PLACEHOLDER_DARWIN_X86_64_SHA256"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/A3S-Lab/Power/releases/download/v0.1.0/a3s-power-0.1.0-linux-arm64.tar.gz"
+      url "https://github.com/A3S-Lab/Power/releases/download/v0.1.1/a3s-power-0.1.1-linux-arm64.tar.gz"
       sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/Power/releases/download/v0.1.0/a3s-power-0.1.0-linux-x86_64.tar.gz"
+      url "https://github.com/A3S-Lab/Power/releases/download/v0.1.1/a3s-power-0.1.1-linux-x86_64.tar.gz"
       sha256 "PLACEHOLDER_LINUX_X86_64_SHA256"
     end
   end
