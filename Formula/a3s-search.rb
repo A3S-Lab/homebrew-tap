9fc63b84195eeba79e2a13496d8423978a2d26f1
RoyLin <1002591652@qq.com>
2026-02-16 00:35:27 +0800

    a3s-search: update to 0.5.5

--- Formula/a3s-search.rb
+++ Formula/a3s-search.rb
@@ -1,28 +1,28 @@
 class A3sSearch < Formula
   desc "Embeddable meta search engine CLI with proxy pool support"
   homepage "https://github.com/A3S-Lab/Search"
-  version "0.5.3"
+  version "0.5.5"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.3/a3s-search-0.5.3-darwin-arm64.tar.gz"
-      sha256 "687f88732b51bceffab54c74017e25c50be3b6675b30b6b9103b09eb2e8bb85b"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.5/a3s-search-0.5.5-darwin-arm64.tar.gz"
+      sha256 "4fe67ce557eb7d819ba535c75fe516eff70c1c90f838b535bf7e366ee97dff63"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.3/a3s-search-0.5.3-darwin-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_X86_64_SHA256"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.5/a3s-search-0.5.5-darwin-x86_64.tar.gz"
+      sha256 "PLACEHOLDER_DARWIN_X86_64"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.3/a3s-search-0.5.3-linux-arm64.tar.gz"
-      sha256 "50785988a86ff7eb120324c7e3b45f222216ec26fe89e812160604f7e4ff4d99"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.5/a3s-search-0.5.5-linux-arm64.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_ARM64"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.3/a3s-search-0.5.3-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X86_64_SHA256"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.5.5/a3s-search-0.5.5-linux-x86_64.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_X86_64"
     end
   end
 
@@ -31,6 +31,6 @@ class A3sSearch < Formula
   end
 
   test do
-    assert_match "A3S Search", shell_output("#{bin}/a3s-search --version")
+    assert_match "a3s-search", shell_output("#{bin}/a3s-search --version")
   end
 end
