28b0ede024f60c6b1d654b40720968ba92d2ea5f
RoyLin <1002591652@qq.com>
2026-02-18 22:22:42 +0800

    a3s-search: update to 0.7.0

--- Formula/a3s-search.rb
+++ Formula/a3s-search.rb
@@ -1,28 +1,28 @@
 class A3sSearch < Formula
   desc "Embeddable meta search engine CLI with proxy pool support"
   homepage "https://github.com/A3S-Lab/Search"
-  version "0.6.0"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.6.0/a3s-search-0.6.0-darwin-arm64.tar.gz"
-      sha256 "cd610af41ddf4464c82a8018fa62f8e1b9650de398349b3ee9c31ef82c17dcdd"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.7.0/a3s-search-0.7.0-darwin-arm64.tar.gz"
+      sha256 "c7ec52916c98c3ea1d0cc4f6b46a14e0887548affe4e85bceef317bfd32da174"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.6.0/a3s-search-0.6.0-darwin-x86_64.tar.gz"
-      sha256 "bccf1b8262d9bd92df18eaec1560eb3ad57ecb28190d18059c09f5ba04745948"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.7.0/a3s-search-0.7.0-darwin-x86_64.tar.gz"
+      sha256 "65b3849d49cbb7a89c9c08258c7dcd13d31737c2b202d91f8c2e2aa41f1aa0b0"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.6.0/a3s-search-0.6.0-linux-arm64.tar.gz"
-      sha256 "3caa1bc4eaa14fbb86bc602c8861a9097e6a3f6465aeb79dab52cdcd405ea7bb"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.7.0/a3s-search-0.7.0-linux-arm64.tar.gz"
+      sha256 "53581d8ba8c1d963da2f0a026aadb6b712d11150d716d835e8e3b92bc88723c0"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/Search/releases/download/v0.6.0/a3s-search-0.6.0-linux-x86_64.tar.gz"
-      sha256 "ffbd46cb5c6b92687f636f420c181fbd84e8982a7824d8a032a8f19192e3f8f7"
+      url "https://github.com/A3S-Lab/Search/releases/download/v0.7.0/a3s-search-0.7.0-linux-x86_64.tar.gz"
+      sha256 "ad921dcf0953094f86050272fd5f8bfeb64223d5e3c20d933a00390593f212e2"
     end
   end
 
