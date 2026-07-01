4183aac2960b63ff74f8cfe9489eeeb906a855b5
Roy Lin <michael.falcone@up.ac.th>
2026-07-01 09:45:34 +0800

    a3s-webview 0.1.1

--- Formula/a3s-webview.rb
+++ Formula/a3s-webview.rb
@@ -1,17 +1,17 @@
 class A3sWebview < Formula
   desc "Native WebView popup helper for the a3s code TUI (RemoteUI)"
   homepage "https://github.com/A3S-Lab/WebView"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.0/a3s-webview-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "75e4a8ca8a1dd3961257841badd63c8f144a3eac1e20aa03f157c65148b3a462"
+      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.1/a3s-webview-v0.1.1-aarch64-apple-darwin.tar.gz"
+      sha256 "82b69c1da24ece031876a3963ffc70c861837b6787069c3693dc8bd26b35af15"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.0/a3s-webview-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "931bd66aadb16f25a1e97b77ae8a374248204c2e349f9cd51b17df07f8d95827"
+      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.1/a3s-webview-v0.1.1-x86_64-apple-darwin.tar.gz"
+      sha256 "39dfd8b78370b61000439cace661156bf0ef3a13772d3d4098dff5a04df16490"
     end
   end
 
