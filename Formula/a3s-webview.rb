b0d893f0e666dafee5240f3257b67ae7f1f17477
A3S CI <ci@a3s.dev>
2026-07-19 07:53:37 +0000

    a3s-webview 0.1.3 (automated)

--- Formula/a3s-webview.rb
+++ Formula/a3s-webview.rb
@@ -1,29 +1,29 @@
 class A3sWebview < Formula
   desc "Native WebView and top-center Agent Island helper for A3S Code"
   homepage "https://github.com/A3S-Lab/WebView"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.2/a3s-webview-v0.1.2-aarch64-apple-darwin.tar.gz"
-      sha256 "862c121ac3b5676a59bf85310d54f686ea87eb7a471791be0d5aa6a0f3c35ecb"
+      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.3/a3s-webview-v0.1.3-aarch64-apple-darwin.tar.gz"
+      sha256 "0dc6387e31f583c56fcdf31f09e6e687346b9e9f7663ac12a500f5ed5b7e60d3"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.2/a3s-webview-v0.1.2-x86_64-apple-darwin.tar.gz"
-      sha256 "2163047088e7adcd8493e17b82912f9c7cb9952db7ed47ad59484faa8b62a7e2"
+      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.3/a3s-webview-v0.1.3-x86_64-apple-darwin.tar.gz"
+      sha256 "5d52564c607c8104e01329ce48cb0adc102fef4a72d71c693c3760849301a8cb"
     end
   end
 
   on_linux do
     depends_on "webkitgtk"
     on_arm do
-      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.2/a3s-webview-v0.1.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "6b1b7dda5e3332947952201fb8c116010fdddd4a47b8883b0604db0c75615f7d"
+      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.3/a3s-webview-v0.1.3-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "0f53b4d41351942b04c007a6c4f2b3096cde66e9c8846f1ccb74a944d42e6df0"
     end
     on_intel do
-      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.2/a3s-webview-v0.1.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2197ef3b0ea9baad4eddfcae00d0ea2d5ecd4fa5f1385dbba29594af234bf3d8"
+      url "https://github.com/A3S-Lab/WebView/releases/download/v0.1.3/a3s-webview-v0.1.3-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "2acc8c8e12a81f0986283db57208a4210db9fca154502f029fb5d7f5ac5147ef"
     end
   end
 
